30fbe5af66fec153a67ed5a9ec4378804949c7dc
Michael Ogala <michael.ogala@andela.com>
2018-02-12 12:22:39 +0100

    Controller for books

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get     'login'        => 'sessions#new',     as: 'login'
   post    'authenticate' => 'sessions#create',  as: 'authenticate'
   delete  'logout'       => 'sessions#destroy', as: 'logout'
+
+  resources :books, only: [:new, :create, :index]
+  get     '/:name'       => 'books#show',       as: 'book'
+  get     '/:name/edit'  => 'books#edit',       as: 'edit_book'
+  put     '/:name'       => 'books#update',     as: 'update_book'
+  delete  '/:name'       => 'books#destroy',    as: 'destroy_book'
 end
