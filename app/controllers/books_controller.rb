b52858afd40c6fc1c6b841d3133ccbc1647ba457
Michael Ogala <michael.ogala@andela.com>
2018-02-12 13:12:48 +0100

    User can now add a new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,17 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = current_user.books.new
   end
 
   def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to books_path, notice: "You have added #{@book.name} your collection"
+    else
+      flash[:alert] = @book.errors
+      render 'new'
+    end
   end
 
   def show
@@ -19,5 +27,15 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book)
+          .permit(
+            :name,
+            :author
+          )
   end
 end
