45efa3164643b997eec2fea26828d3a523fd4eb8
Michael Ogala <michael.ogala@andela.com>
2018-02-12 13:42:41 +0100

    Display the name of the user who added the book

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :books
   validates :first_name, :last_name, :username, presence: true
   has_secure_password
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
